008907cc9c075f5c7863aeab056d1a275ef99e5c
SeongJin Jeong <jsj0718tjdwl@naver.com>
2020-05-07 22:51:54 +0900

    Added Data Show

--- src/scripts/pages/Teams.js
+++ src/scripts/pages/Teams.js
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Top from "../public/TopBar";
 import { useParams } from "react-router-dom";
+import callApi from "../../fetchApi";
+
+const Call = callApi;
 
 const Teams = () => {
   let { id } = useParams();
-  console.log(id);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    Call._callTeam(id).then((res) => setData(res.api.teams[0]));
+  }, []);
   return (
     <>
-      {/* <Top /> */}
-      <span>hello</span>
+      <Top />
+      <RenderTeamInfo data={data} />
+    </>
+  );
+};
+
+const RenderTeamInfo = (props) => {
+  const data = props.data;
+  return (
+    <>
+      <img src={data.logo} alt={data.name} />
+      <p>Name : {data.name}</p>
+      <p>Code : {data.code}</p>
+      <p>Country : {data.country}</p>
+      <p>Founded Date : {data.founded}</p>
+      <p>Venue Name : {data.venue_name}</p>
+      <p>Venue Address : {data.venue_address}</p>
     </>
   );
 };
