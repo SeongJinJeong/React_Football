56d4ad218a614480216699b75d69f1f486ad8dfd
SeongJin <jsj0718tjdwl@naver.com>
2020-05-11 22:13:50 +0900

    Added some Infos in the Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import TeamRoute from "./routes/index";
 import { BrowserRouter as Router } from "react-router-dom";
 import callApi from "./fetchApi";
 
+import Moment from 'react-moment';
+import 'moment-timezone';
+
 const Call = callApi;
 
 function App() {
--- src/scripts/pages/Fixtures.js
+++ src/scripts/pages/Fixtures.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import Moment from "react-moment";
 
 import Top from "../public/TopBar";
 import Main from "../public/Main";
@@ -11,16 +12,18 @@ const Call = callApi;
 const Fixtures = (props) => {
   let { id: teamId } = useParams();
   const [fixtures, setFixtures] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
-    Call._callTeamFixture(teamId).then((res) =>
-      setFixtures(res.api.fixtures.reverse())
-    );
+    Call._callTeamFixture(teamId).then((res) => {
+      setFixtures(res.api.fixtures.reverse());
+      setLoading(false);
+    });
   }, []);
 
   return (
     <>
       <Top />
-      <RenderFixtures fixtures={fixtures} />
+      {loading ? "Now Loading" : <RenderFixtures fixtures={fixtures} />}
     </>
   );
 };
@@ -28,22 +31,94 @@ const Fixtures = (props) => {
 const RenderFixtures = (props) => {
   const fixtures = props.fixtures;
   const futureFixtures = fixtures.filter((data) => {
-    if (data.event_timestamp > Date.now() / 1000) return data;
-  }).reverse();
-  console.log(futureFixtures)
+    if (data.event_timestamp < Date.now() / 1000) return data;
+  });
+  // .reverse();
+  console.log(futureFixtures);
   return futureFixtures.map((value, index) => {
     return (
-      <Div>
-        <Team away>
-          <img src={value.awayTeam.logo} style={{width:"30%",height:"50%"}}/>
-          <p style={{ fontWeight: "bold", fontSize:"30px" }}>{value.awayTeam.team_name}</p>
-        </Team>
-        {/* <p style={{fontWeight:"bold",fontSize:"50px",float:"unset"}}>VS</p> */}
-        <Team>
-          <img src={value.homeTeam.logo} style={{width:"30%",height:"50%"}}/>
-          <p style={{ fontWeight: "bold", color:"white", fontSize:"30px" }}>{value.homeTeam.team_name}</p>
-        </Team>
-      </Div>
+      <>
+        <Div>
+          <Team>
+            <img
+              src={value.homeTeam.logo}
+              style={{ maxWidth: "100px", maxHeight: "100px" }}
+            />
+            <p style={{ fontWeight: "bold", color: "white", fontSize: "30px" }}>
+              {value.homeTeam.team_name}
+            </p>
+          </Team>
+          <Team away>
+            <img
+              src={value.awayTeam.logo}
+              style={{ maxWidth: "100px", maxHeight: "100px" }}
+            />
+            <p style={{ fontWeight: "bold", fontSize: "30px" }}>
+              {value.awayTeam.team_name}
+            </p>
+          </Team>
+        </Div>
+        <Div style={{ margin: 0 }}>
+          <TeamDesc left>
+            <h2
+              style={{
+                padding: "5px",
+                border: "3px solid blue",
+                borderRadius: "5px 5px 5px 5px",
+              }}
+            >
+              Home
+            </h2>
+            <h1>
+              {value.goalsHomeTeam !== null ? value.goalsHomeTeam : "NONE"}
+            </h1>
+          </TeamDesc>
+          <TeamDesc>
+            <h2
+              style={{
+                padding: "5px",
+                border: "3px solid red",
+                borderRadius: "5px 5px 5px 5px",
+              }}
+            >
+              Away
+            </h2>
+            <h1>
+              {value.goalsAwayTeam !== null ? value.goalsAwayTeam : "NONE"}
+            </h1>
+          </TeamDesc>
+        </Div>
+        <Div info>
+          <MatchInfo>
+            <p style={{ margin: 0 }}>
+              <b>Event Date</b> :{" "}
+              <Moment
+                interval={0}
+                date={value.event_date}
+                format="YYYY/MM/DD"
+              ></Moment>
+            </p>
+            <p style={{ margin: 0 }}>
+              <b>Status</b> : {value.status}
+            </p>
+            <p style={{ margin: 0 }}>
+              <b>Referee</b> : {value.referee !== null ? value.referee : "NONE"}
+            </p>
+            <p />
+            <p style={{ margin: 0 }}>
+              <b>League</b> :{" "}
+              {value.league.name !== null ? value.league.name : "NONE"}
+            </p>
+            <p style={{ margin: 0 }}>
+              <b>Round</b> : {value.round !== null ? value.round : "NONE"}
+            </p>
+            <p />
+            <p style={{ margin: 0 }}>
+              <b>Venue</b> : {value.venue !== null ? value.venue : "NONE"}
+            </p>
+          </MatchInfo>
+        </Div>
+      </>
     );
   });
 };
@@ -55,7 +130,7 @@ const Div = styled.div`
   justify-content: center;
   align-items: center;
 
-  margin: 20px 0px;
+  margin-top: ${(props) => (props.info ? null : 20)}px;
 `;
 
 const Team = styled.div`
@@ -67,12 +142,32 @@ const Team = styled.div`
   justify-content: center;
   align-items: center;
 
-  background-color : ${props=>props.away?"#f79071":"#342ead"};
+  background-color: ${(props) => (props.away ? "#f79071" : "#342ead")};
 
-  padding :20px;
+  padding: 20px;
 
-  min-height : 300px;
-  max-height : 300px;
+  min-height: 300px;
+  max-height: 300px;
+`;
+
+const TeamDesc = styled(Team)`
+  background-color: white;
+
+  border-right: ${(props) => (props.left ? "1px dashed black" : null)};
+`;
+
+const MatchInfo = styled.div`
+  width: 80%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 20px;
+
+  border: 2px solid black;
+  border-radius: 10px;
 `;
 
 export default Fixtures;
